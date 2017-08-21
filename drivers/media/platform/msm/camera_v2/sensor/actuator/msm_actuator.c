8ee9426d30bde1e426532c37b95f2b07b107e6c4
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2017-08-21 01:56:03 -0500

    land:platform:add missing camera steps
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -1,4 +1,5 @@
 /* Copyright (c) 2011-2017, The Linux Foundation. All rights reserved.
+ * Copyright (C) 2016 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -902,10 +903,8 @@ static int32_t msm_actuator_bivcm_init_step_table(
 		code_per_step =
 			a_ctrl->region_params[region_index].code_per_step;
 		step_boundary =
-			a_ctrl->region_params[region_index].
-			step_bound[MOVE_NEAR];
-		if (step_boundary >
-			set_info->af_tuning_params.total_steps) {
+		    a_ctrl->region_params[region_index].step_bound[MOVE_NEAR];
+		if (step_boundary > set_info->af_tuning_params.total_steps) {
 			pr_err("invalid step_boundary = %d, max_val = %d",
 				step_boundary,
 				set_info->af_tuning_params.total_steps);
@@ -956,8 +955,8 @@ static int32_t msm_actuator_init_step_table(struct msm_actuator_ctrl_t *a_ctrl,
 
 	/* validate the actuator state */
 	if (a_ctrl->actuator_state != ACT_OPS_ACTIVE) {
-		pr_err("%s:%d invalid actuator_state %d\n"
-			, __func__, __LINE__, a_ctrl->actuator_state);
+		pr_err("%s:%d invalid actuator_state %d\n", __func__, __LINE__,
+		       a_ctrl->actuator_state);
 		return -EINVAL;
 	}
 	for (; data_size > 0; data_size--)
@@ -993,10 +992,8 @@ static int32_t msm_actuator_init_step_table(struct msm_actuator_ctrl_t *a_ctrl,
 		qvalue =
 			a_ctrl->region_params[region_index].qvalue;
 		step_boundary =
-			a_ctrl->region_params[region_index].
-			step_bound[MOVE_NEAR];
-		if (step_boundary >
-			set_info->af_tuning_params.total_steps) {
+			a_ctrl->region_params[region_index].step_bound[MOVE_NEAR];
+		if (step_boundary > set_info->af_tuning_params.total_steps) {
 			pr_err("invalid step_boundary = %d, max_val = %d",
 				step_boundary,
 				set_info->af_tuning_params.total_steps);
@@ -1004,8 +1001,7 @@ static int32_t msm_actuator_init_step_table(struct msm_actuator_ctrl_t *a_ctrl,
 			a_ctrl->step_position_table = NULL;
 			return -EINVAL;
 		}
-		for (; step_index <= step_boundary;
-			step_index++) {
+		for (; step_index <= step_boundary; step_index++) {
 			if (qvalue > 1 && qvalue <= MAX_QVALUE)
 				cur_code = step_index * code_per_step / qvalue;
 			else
