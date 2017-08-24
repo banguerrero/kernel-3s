bec8b818ccdaa5b21909b1cba40f237902f85b46
nichcream <c.potato23@gmail.com>
2017-08-25 00:13:53 +0800

    msm: camera_land_v2: Import default camera stack for land
    Taken from LA.UM.5.1-03810-8x37.0 release with Xiaomi's patches.
    Use an older camera stack due to some compatibility issues with the newer camera stack.

--- include/media/msmb_pproc.h
+++ include/media/msmb_pproc.h
@@ -1,3 +1,6 @@
+#ifdef CONFIG_LAND_CAMERA
+#include "msmb_pproc_land.h"
+#else
 #ifndef __MSMB_PPROC_H
 #define __MSMB_PPROC_H
 
@@ -152,4 +155,4 @@ struct msm_camera_v4l2_ioctl32_t {
 #endif
 
 #endif
-
+#endif
