e153e1bb79402d4c6ba5d5e189ea8da829c10a21
klozz <klozz707@gmail.com>
2017-11-14 01:44:37 -0500

    fix derp
    Change-Id: Ie2e51bbf602276260f53ea99e4893439c03e5624
    Signed-off-by: klozz <klozz707@gmail.com>

--- include/linux/compiler-gcc.h
+++ include/linux/compiler-gcc.h
@@ -114,15 +114,7 @@
  * would be.
  * [...]
  */
-<<<<<<< HEAD
-#define __pure			__attribute__((pure))
-#define __aligned(x)		__attribute__((aligned(x)))
-#define __printf(a, b)		__attribute__((format(printf, a, b)))
-#define __scanf(a, b)		__attribute__((format(scanf, a, b)))
-#define __attribute_const__	__attribute__((__const__))
-#define __maybe_unused		__attribute__((unused))
-#define __always_unused		__attribute__((unused))
-=======
+
 #define __pure				__attribute__((pure))
 #define __aligned(x)			__attribute__((aligned(x)))
 #define __printf(a, b)			__attribute__((format(printf, a, b)))
@@ -131,7 +123,6 @@
 #define __attribute_const__		__attribute__((__const__))
 #define __maybe_unused			__attribute__((unused))
 #define __always_unused			__attribute__((unused))
->>>>>>> 677fa15... compiler-gcc: integrate the various compiler-gcc[345].h files
 
 /* gcc version specific checks */
 
@@ -210,7 +201,7 @@
 #define unreachable() __builtin_unreachable()
 
 /* Mark a function definition as prohibited from being cloned. */
-#define __noclone	__attribute__((__noclone__, __optimize__("no-tracer")))
+#define __noclone	__attribute__((__noclone__))
 
 #endif /* GCC_VERSION >= 40500 */
 
